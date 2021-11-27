7d715f3603207343dd8be51e7137b8e2c4b131f8
ObedDecath <obed.oseifrimpong@decathlon.com>
2021-11-27 19:06:36 +0800

    Updates

---
+++ _next/static/YZo9HBEE2LpOtiFjRzB9H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-840cc68ffd38c219303a.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
